4ca2368b03953bdff8bc862e9b7c007d4a2ff9ec
ShenQiangTestLovers <18013005698@163.com>
2019-12-08 21:42:51 +0800

    新增moco框架

---
+++ tryApiTest/mockTest/mocoIntroduction.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : shenqiang
+'''
+moco地址：https://github.com/dreamhead/moco
+api文档地址：
+https://github.com/dreamhead/moco/blob/master/moco-doc/apis.md
+启动命令：
+java -jar moco-runner-0.12.0-standalone.jar http -p 12306 -c foo.json
+moco只关注服务器的配置，也就是客户端与服务端，或者更加具体的说就是请求和响应。
+
+'''
+
+
+
+
